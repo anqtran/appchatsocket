1dcf5acd7ab476606c72e8945d16f841bcc49735
An Q Tran <anqtran@gatech.edu>
2018-02-11 11:46:46 +0100

    Set up Socket IO

---
+++ client.js
@@ -0,0 +1,6 @@
+$(function () {
+
+    // Connect to socket.io
+    var socket = io.connect('http://127.0.0.1:4000');
+
+});
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
+const io = require('socket.io').listen(4000);
 
 let url = 'mongodb://127.0.0.1/socketchat';
 
@@ -13,5 +14,16 @@ MongoClient.connect(url, function (err, db) {
     const socketchat = db.db('socketchat');
     const users = socketchat.collection('users');
     const messages = socketchat.collection('messages');
+
+
+    /*
+    * Connect to socket.io
+    */
+    io.on('connection', function (socket) {
+
+        console.log('Connected to socket.io, ID: ' + socket.id);
+
+    });
+
 
 });
